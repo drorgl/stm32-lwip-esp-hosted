ea1f0c1cd22de0d9eb7340588e2e44497e94d116
Dror Gluska <drorgl@yahoo.com>
2021-11-14 07:55:35 +0200

    implement uart buffer using DMA

--- stm32-lwip/Core/Src/usart.c
+++ stm32-lwip/Core/Src/usart.c
@@ -1,21 +1,21 @@
 /**
- ******************************************************************************
- * @file    usart.c
- * @brief   This file provides code for the configuration
- *          of the USART instances.
- ******************************************************************************
- * @attention
- *
- * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
- * All rights reserved.</center></h2>
- *
- * This software component is licensed by ST under Ultimate Liberty license
- * SLA0044, the "License"; You may not use this file except in compliance with
- * the License. You may obtain a copy of the License at:
- *                             www.st.com/SLA0044
- *
- ******************************************************************************
- */
+  ******************************************************************************
+  * @file    usart.c
+  * @brief   This file provides code for the configuration
+  *          of the USART instances.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
 
 /* Includes ------------------------------------------------------------------*/
 #include "usart.h"
@@ -23,6 +23,8 @@
 /* USER CODE BEGIN 0 */
 #include <cmsis_os.h>
 #include "ringbuffer/ring_buffer.h"
+
+static osSemaphoreId writeSemaphore;
 
 #define TX_DMA_BUFFER_SIZE 16
 __aligned(32) uint8_t TX_DMA_buffer[TX_DMA_BUFFER_SIZE];
@@ -37,6 +39,9 @@ TM_BUFFER_t tx_buffer;
 uint8_t tx_buffer_container[255];
 
 void initialize_buffers(void) {
+	osSemaphoreDef(WRITESEM);
+	writeSemaphore = osSemaphoreCreate(osSemaphore(WRITESEM) , 1);
+
 	TM_BUFFER_Init(&rx_buffer, sizeof(rx_buffer_container),
 			rx_buffer_container);
 	TM_BUFFER_Init(&tx_buffer, sizeof(tx_buffer_container),
@@ -51,124 +56,132 @@ DMA_HandleTypeDef hdma_usart2_tx;
 
 /* USART2 init function */
 
-void MX_USART2_UART_Init(void) {
+void MX_USART2_UART_Init(void)
+{
 
-	/* USER CODE BEGIN USART2_Init 0 */
+  /* USER CODE BEGIN USART2_Init 0 */
 
-	/* USER CODE END USART2_Init 0 */
+  /* USER CODE END USART2_Init 0 */
 
-	/* USER CODE BEGIN USART2_Init 1 */
+  /* USER CODE BEGIN USART2_Init 1 */
 
-	/* USER CODE END USART2_Init 1 */
-	huart2.Instance = USART2;
-	huart2.Init.BaudRate = 115200;
-	huart2.Init.WordLength = UART_WORDLENGTH_8B;
-	huart2.Init.StopBits = UART_STOPBITS_1;
-	huart2.Init.Parity = UART_PARITY_NONE;
-	huart2.Init.Mode = UART_MODE_TX_RX;
-	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-	huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-	if (HAL_UART_Init(&huart2) != HAL_OK) {
-		Error_Handler();
-	}
-	/* USER CODE BEGIN USART2_Init 2 */
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
 	HAL_UARTEx_ReceiveToIdle_DMA(&huart2, RX_DMA_buffer, RX_DMA_BUFFER_SIZE);
 	__HAL_DMA_DISABLE_IT(&hdma_usart2_rx, DMA_IT_HT);
-	/* USER CODE END USART2_Init 2 */
+  /* USER CODE END USART2_Init 2 */
 
 }
 
-void HAL_UART_MspInit(UART_HandleTypeDef *uartHandle) {
+void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
+{
 
-	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
-	if (uartHandle->Instance == USART2) {
-		/* USER CODE BEGIN USART2_MspInit 0 */
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(uartHandle->Instance==USART2)
+  {
+  /* USER CODE BEGIN USART2_MspInit 0 */
 		initialize_buffers();
-		/* USER CODE END USART2_MspInit 0 */
-		/* USART2 clock enable */
-		__HAL_RCC_USART2_CLK_ENABLE();
+  /* USER CODE END USART2_MspInit 0 */
+    /* USART2 clock enable */
+    __HAL_RCC_USART2_CLK_ENABLE();
 
-		__HAL_RCC_GPIOA_CLK_ENABLE();
-		/**USART2 GPIO Configuration
-		 PA2     ------> USART2_TX
-		 PA3     ------> USART2_RX
-		 */
-		GPIO_InitStruct.Pin = USART_TX_Pin | USART_RX_Pin;
-		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-		GPIO_InitStruct.Pull = GPIO_NOPULL;
-		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-		GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
-		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**USART2 GPIO Configuration
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX
+    */
+    GPIO_InitStruct.Pin = USART_TX_Pin|USART_RX_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-		/* USART2 DMA Init */
-		/* USART2_RX Init */
-		hdma_usart2_rx.Instance = DMA1_Stream5;
-		hdma_usart2_rx.Init.Channel = DMA_CHANNEL_4;
-		hdma_usart2_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
-		hdma_usart2_rx.Init.PeriphInc = DMA_PINC_DISABLE;
-		hdma_usart2_rx.Init.MemInc = DMA_MINC_ENABLE;
-		hdma_usart2_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-		hdma_usart2_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-		hdma_usart2_rx.Init.Mode = DMA_NORMAL;
-		hdma_usart2_rx.Init.Priority = DMA_PRIORITY_LOW;
-		hdma_usart2_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
-		if (HAL_DMA_Init(&hdma_usart2_rx) != HAL_OK) {
-			Error_Handler();
-		}
+    /* USART2 DMA Init */
+    /* USART2_RX Init */
+    hdma_usart2_rx.Instance = DMA1_Stream5;
+    hdma_usart2_rx.Init.Channel = DMA_CHANNEL_4;
+    hdma_usart2_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    hdma_usart2_rx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_usart2_rx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_usart2_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_usart2_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_usart2_rx.Init.Mode = DMA_NORMAL;
+    hdma_usart2_rx.Init.Priority = DMA_PRIORITY_LOW;
+    hdma_usart2_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    if (HAL_DMA_Init(&hdma_usart2_rx) != HAL_OK)
+    {
+      Error_Handler();
+    }
 
-		__HAL_LINKDMA(uartHandle, hdmarx, hdma_usart2_rx);
+    __HAL_LINKDMA(uartHandle,hdmarx,hdma_usart2_rx);
 
-		/* USART2_TX Init */
-		hdma_usart2_tx.Instance = DMA1_Stream6;
-		hdma_usart2_tx.Init.Channel = DMA_CHANNEL_4;
-		hdma_usart2_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
-		hdma_usart2_tx.Init.PeriphInc = DMA_PINC_DISABLE;
-		hdma_usart2_tx.Init.MemInc = DMA_MINC_ENABLE;
-		hdma_usart2_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-		hdma_usart2_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-		hdma_usart2_tx.Init.Mode = DMA_NORMAL;
-		hdma_usart2_tx.Init.Priority = DMA_PRIORITY_LOW;
-		hdma_usart2_tx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
-		if (HAL_DMA_Init(&hdma_usart2_tx) != HAL_OK) {
-			Error_Handler();
-		}
+    /* USART2_TX Init */
+    hdma_usart2_tx.Instance = DMA1_Stream6;
+    hdma_usart2_tx.Init.Channel = DMA_CHANNEL_4;
+    hdma_usart2_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+    hdma_usart2_tx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_usart2_tx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_usart2_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_usart2_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_usart2_tx.Init.Mode = DMA_NORMAL;
+    hdma_usart2_tx.Init.Priority = DMA_PRIORITY_LOW;
+    hdma_usart2_tx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    if (HAL_DMA_Init(&hdma_usart2_tx) != HAL_OK)
+    {
+      Error_Handler();
+    }
 
-		__HAL_LINKDMA(uartHandle, hdmatx, hdma_usart2_tx);
+    __HAL_LINKDMA(uartHandle,hdmatx,hdma_usart2_tx);
 
-		/* USART2 interrupt Init */
-		HAL_NVIC_SetPriority(USART2_IRQn, 5, 0);
-		HAL_NVIC_EnableIRQ(USART2_IRQn);
-		/* USER CODE BEGIN USART2_MspInit 1 */
+    /* USART2 interrupt Init */
+    HAL_NVIC_SetPriority(USART2_IRQn, 5, 0);
+    HAL_NVIC_EnableIRQ(USART2_IRQn);
+  /* USER CODE BEGIN USART2_MspInit 1 */
 
-		/* USER CODE END USART2_MspInit 1 */
-	}
+  /* USER CODE END USART2_MspInit 1 */
+  }
 }
 
-void HAL_UART_MspDeInit(UART_HandleTypeDef *uartHandle) {
+void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
+{
 
-	if (uartHandle->Instance == USART2) {
-		/* USER CODE BEGIN USART2_MspDeInit 0 */
+  if(uartHandle->Instance==USART2)
+  {
+  /* USER CODE BEGIN USART2_MspDeInit 0 */
 
-		/* USER CODE END USART2_MspDeInit 0 */
-		/* Peripheral clock disable */
-		__HAL_RCC_USART2_CLK_DISABLE();
+  /* USER CODE END USART2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_USART2_CLK_DISABLE();
 
-		/**USART2 GPIO Configuration
-		 PA2     ------> USART2_TX
-		 PA3     ------> USART2_RX
-		 */
-		HAL_GPIO_DeInit(GPIOA, USART_TX_Pin | USART_RX_Pin);
+    /**USART2 GPIO Configuration
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX
+    */
+    HAL_GPIO_DeInit(GPIOA, USART_TX_Pin|USART_RX_Pin);
 
-		/* USART2 DMA DeInit */
-		HAL_DMA_DeInit(uartHandle->hdmarx);
-		HAL_DMA_DeInit(uartHandle->hdmatx);
+    /* USART2 DMA DeInit */
+    HAL_DMA_DeInit(uartHandle->hdmarx);
+    HAL_DMA_DeInit(uartHandle->hdmatx);
 
-		/* USART2 interrupt Deinit */
-		HAL_NVIC_DisableIRQ(USART2_IRQn);
-		/* USER CODE BEGIN USART2_MspDeInit 1 */
+    /* USART2 interrupt Deinit */
+    HAL_NVIC_DisableIRQ(USART2_IRQn);
+  /* USER CODE BEGIN USART2_MspDeInit 1 */
 
-		/* USER CODE END USART2_MspDeInit 1 */
-	}
+  /* USER CODE END USART2_MspDeInit 1 */
+  }
 }
 
 /* USER CODE BEGIN 1 */
@@ -177,8 +190,10 @@ static int tx_next_chunk(void) {
 	int number_of_items_in_tx_buffer = TM_BUFFER_Read(&tx_buffer, TX_DMA_buffer,
 			TX_DMA_BUFFER_SIZE);
 	if (number_of_items_in_tx_buffer > 0) {
-		HAL_UART_Transmit_DMA(&huart2, TX_DMA_buffer,
-				number_of_items_in_tx_buffer);
+		if (HAL_UART_Transmit_DMA(&huart2, TX_DMA_buffer,
+				number_of_items_in_tx_buffer) != HAL_OK){
+			assert(0);
+		}
 		__HAL_DMA_DISABLE_IT(&hdma_usart2_rx, DMA_IT_HT);
 	}
 	return number_of_items_in_tx_buffer;
@@ -205,7 +220,8 @@ void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
 }
 
 void put_tx_data_with_wait(uint8_t *buffer, size_t buffer_length) {
-	while (1) {
+	int retries = 1000;
+	while (retries > 0) {
 		int pushed_bytes = put_tx_data(buffer, buffer_length);
 		buffer_length -= pushed_bytes;
 		buffer += pushed_bytes;
@@ -213,12 +229,16 @@ void put_tx_data_with_wait(uint8_t *buffer, size_t buffer_length) {
 			break;
 		}
 		osDelay(1);
+		retries--;
 	}
 }
 
 int put_tx_data(uint8_t *buffer, size_t buffer_length) {
-
-	int ret = TM_BUFFER_Write(&tx_buffer, buffer, buffer_length);
+	int ret = 0;
+	if (osSemaphoreWait(writeSemaphore , osWaitForever) == osOK){
+		ret = TM_BUFFER_Write(&tx_buffer, buffer, buffer_length);
+		osSemaphoreRelease(writeSemaphore);
+	}
 
 	if (huart2.gState == HAL_UART_STATE_READY) {
 		tx_next_chunk();
